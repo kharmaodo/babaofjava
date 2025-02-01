d6db27e650ee89062ebf3dd12ee70f85828624e1
Maodo DIOP <maodo.diop.external@worldline.com>
2025-02-01 11:03:03 +0000

    feat: interface sealed with permits

--- src/main/java/sn/edu/sid/bambey/HomeWorker.java
+++ src/main/java/sn/edu/sid/bambey/HomeWorker.java
@@ -3,9 +3,8 @@ package sn.edu.sid.bambey;
  * 
  * HomeWorker est un human mais pas un Student qui est sensé payer une location
  */
-public record HomeWorker(Integer idHourly,  Human humanDetails) implements LocationPayment {
+public record HomeWorker(Integer idHourly,  Human humanDetails) {
 
-@Override
 public String paymentLocation() {
 	return "Paiement fait par le Homeworker ["+humanDetails.firstName()+"]  ["+humanDetails.lastName()+ " ] avec id journalier ["+this.idHourly()+"]";
 }
